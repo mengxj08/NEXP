bc6b06359b58fbff587f7eee12cc87c644a2aaf4
xiaojun <mengxj.nus@gmail.com>
2013-08-22 18:52:02 +0800

    Add ImageSources

--- NEXP/NEXP/Utils/OpaqueClickableImage.cs
+++ NEXP/NEXP/Utils/OpaqueClickableImage.cs
@@ -16,13 +16,27 @@ namespace NEXP.Utils
         {
             var source = (BitmapSource)Source;
 
+            Log.getLogInstance().writeLog("source.PixelWidth:" + source.PixelWidth);
+            Log.getLogInstance().writeLog("source.PixelHeight:" + source.PixelHeight);
+            Log.getLogInstance().writeLog("hitTestParameters.HitPoint.X:" + hitTestParameters.HitPoint.X);
+            Log.getLogInstance().writeLog("hitTestParameters.HitPoint.Y:" + hitTestParameters.HitPoint.Y);
+            Log.getLogInstance().writeLog("ActualWidth:" + ActualWidth);
+            Log.getLogInstance().writeLog("ActualHeight:" + ActualHeight);
             // Get the pixel of the source that was hit
             var x = (int)(hitTestParameters.HitPoint.X / ActualWidth * source.PixelWidth);
             var y = (int)(hitTestParameters.HitPoint.Y / ActualHeight * source.PixelHeight);
 
             // Copy the single pixel into a new byte array representing RGBA
             var pixel = new byte[4];
-            source.CopyPixels(new Int32Rect(x, y, 1, 1), pixel, 4, 0);
+            try
+            {
+                source.CopyPixels(new Int32Rect(x, y, 1, 1), pixel, 4, 0);
+            }
+            catch(ArgumentException e)
+            {
+              return null;
+            }
+            
 
             // Check the alpha (transparency) of the pixel
             // - threshold can be adjusted from 0 to 255
