1b82b401889d0fbb32b002e7217162eb60af8281
xiaojun <mengxj.nus@gmail.com>
2013-08-30 13:33:54 +0800

    IDV add and del DONE

--- NEXP/NEXP/Content/IDV.xaml.cs
+++ NEXP/NEXP/Content/IDV.xaml.cs
@@ -23,7 +23,7 @@ namespace NEXP.Content
         private DateTime downTime;
         private object downSender;
         private Point downPosition;
-
+        
         public IDV()
         {
             InitializeComponent();
@@ -67,27 +67,33 @@ namespace NEXP.Content
         }
         private void add_item(object sender, RoutedEventArgs e)
         {
-            if (addItemText.Text != "")
+            if (addItemText.Text == "")
             {
+                MessageBoxButton btn = MessageBoxButton.OK;
+                FirstFloor.ModernUI.Windows.Controls.ModernDialog.ShowMessage("You should fill in the Input TextArea below before adding!", "Error Message", btn);
+                return;
+            }
+            //if (addItemText.Text != "")
+            //{
                 //if (tv.SelectedItem == null)
                 //{
-                    NEXP.IndependentVariable idv = new NEXP.IndependentVariable();
-                    idv.name = addItemText.Text;
-                    NEXP.MainWindow.datas.independentVariables.Add(idv);
+                NEXP.IndependentVariable idv = new NEXP.IndependentVariable();
+                idv.name = addItemText.Text;
+                NEXP.MainWindow.datas.independentVariables.Add(idv);
                 //}
-                    /*
-                else if (tv.Items.IndexOf(tv.SelectedItem) != -1)
-                {
-                    NEXP.IndependentVariable selectedIDV = (NEXP.IndependentVariable)tv.SelectedItem;
-                    NEXP.IndependentVariable.Level level = new NEXP.IndependentVariable.Level();
-                    level.name = addItemText.Text;
-                    selectedIDV.levels.Add(level);
-                }
-                else
-                {
-                }
-                     * */
+                /*
+            else if (tv.Items.IndexOf(tv.SelectedItem) != -1)
+            {
+                NEXP.IndependentVariable selectedIDV = (NEXP.IndependentVariable)tv.SelectedItem;
+                NEXP.IndependentVariable.Level level = new NEXP.IndependentVariable.Level();
+                level.name = addItemText.Text;
+                selectedIDV.levels.Add(level);
             }
+            else
+            {
+            }
+                 * */
+            //}
         }
         private void Icon_Add_MouseDown(object sender, MouseButtonEventArgs e)
         {
@@ -150,17 +156,118 @@ namespace NEXP.Content
                                         //Log.getLogInstance().writeLog("second");
                                         IDV.levels.Remove(selectedLevel);
                                     }
-
                                 }
-
                             }
                         }
                     }
-                    else { 
+                    else
+                    {
+                    }
+                }
+            }
+        }
+        private void Item_Add(object sender, RoutedEventArgs e)
+        {
+            if (addItemText.Text == "")
+            {
+                MessageBoxButton btn = MessageBoxButton.OK;
+                FirstFloor.ModernUI.Windows.Controls.ModernDialog.ShowMessage("You should fill in the Input TextArea below before adding!", "Error Message", btn);
+                return;
+            }
+            TreeViewItem currentTreeViewItem = null;
+
+            //Find the original Source and set it as the currentTreeViewItem
+            FrameworkElement element = e.OriginalSource as FrameworkElement;
+            if (element != null)
+            {
+                while (VisualTreeHelper.GetParent(element) != null)
+                {
+                    element = VisualTreeHelper.GetParent(element) as FrameworkElement;
+                    TreeViewItem item = element as TreeViewItem;
+                    if (item != null)
+                    {
+                        // Perform custom logic here
+                        // You have to return here because otherwise the method will traverse
+                        // the whole visual tree on every mouse move and there will be performance
+                        // implications
+                        currentTreeViewItem = item;
+                        break;
+                    }
+                }
+            }
+
+            currentTreeViewItem.IsSelected = true;//Mark the current TreeViewItem as the selectedItem
+            if (tv.Items.IndexOf(tv.SelectedItem) != -1)
+            {
+                NEXP.IndependentVariable selectedIDV = (NEXP.IndependentVariable)tv.SelectedItem;
+                NEXP.IndependentVariable.Level level = new NEXP.IndependentVariable.Level();
+                level.name = addItemText.Text;
+                selectedIDV.levels.Add(level);
+                currentTreeViewItem.IsExpanded = true;
+            }
+            else
+            {
+                NEXP.IndependentVariable.Level selectedLevel = (NEXP.IndependentVariable.Level)tv.SelectedItem;
+                NEXP.IndependentVariable.Level level = new NEXP.IndependentVariable.Level();
+                level.name = addItemText.Text;
+                foreach (NEXP.IndependentVariable IDV in NEXP.MainWindow.datas.independentVariables)
+                {
+                    if (IDV.levels.Contains(selectedLevel))
+                    {
+                        //Log.getLogInstance().writeLog("second");
+                        IDV.levels.Add(level);
+                        break;
+                    }
+                }
+                currentTreeViewItem.IsExpanded = true;
+            }
+
+        }
+        private void Item_Del(object sender, RoutedEventArgs e)
+        {
+            TreeViewItem currentTreeViewItem = null;
+
+            //Find the original Source and set it as the currentTreeViewItem
+            FrameworkElement element = e.OriginalSource as FrameworkElement;
+            if (element != null)
+            {
+                while (VisualTreeHelper.GetParent(element) != null)
+                {
+                    element = VisualTreeHelper.GetParent(element) as FrameworkElement;
+                    TreeViewItem item = element as TreeViewItem;
+                    if (item != null)
+                    {
+                        // Perform custom logic here
+                        // You have to return here because otherwise the method will traverse
+                        // the whole visual tree on every mouse move and there will be performance
+                        // implications
+                        currentTreeViewItem = item;
+                        break;
                     }
                 }
             }
             
+            currentTreeViewItem.IsSelected = true;//Mark the current TreeViewItem as the selectedItem
+            if (tv.Items.IndexOf(tv.SelectedItem) != -1)
+            {
+
+                NEXP.MainWindow.datas.independentVariables.RemoveAt(tv.Items.IndexOf(tv.SelectedItem));
+                return;
+            }
+            else
+            {
+                NEXP.IndependentVariable.Level tmp = (NEXP.IndependentVariable.Level)tv.SelectedItem;
+                foreach (NEXP.IndependentVariable IDV in NEXP.MainWindow.datas.independentVariables)
+                {
+                    if (IDV.levels.Contains(tmp))
+                    {
+                        //Log.getLogInstance().writeLog("second");
+                        IDV.levels.Remove(tmp);
+                    }
+
+                }
+
+            }
         }
     }
 }
